adcb223405512d299b95d0939eed7eb32c51ace3
Evan Boehs <ebrobot2000@gmail.com>
2021-03-12 15:54:06 -0500

    ye

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 function fade(element, element2) {
 	let h1Elements = document.getElementById("div");
 	h1Elements.classList.toggle("fade");
@@ -5,4 +9,13 @@ function fade(element, element2) {
 	f1.classList.toggle("fade");
 	let f2 = document.getElementById(element2);
 	f2.classList.toggle("fade");
+}
+
+async function on(overlay) {
+	await sleep(1900);
+	document.getElementById(overlay).style.display = "block";
+}
+
+async function off(overlay) {
+	document.getElementById(overlay).style.display = "none";
 }
